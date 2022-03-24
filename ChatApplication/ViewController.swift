b837a5abf976f2bc04ced265d3ccd35eaf655753
Gali Srikanth <GaliSrikanth19@gmail.coom>
2022-03-24 09:05:12 +0530

    final code

--- ChatApplication/ViewController.swift
+++ ChatApplication/ViewController.swift
@@ -1,19 +1,62 @@
 //
 //  ViewController.swift
-//  ChatApplication
+//  ChatSample
 //
-//  Created by Elorce on 24/03/22.
+//  Created by Hafiz on 20/09/2019.
+//  Copyright © 2019 Nibs. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var messages = [Message]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // set title
+        title = "Lorem Ipsum"
+        setupTable()
+        fetchData()
     }
+    
+    func setupTable() {
+        // config tableView
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.dataSource = self
+        tableView.backgroundColor = UIColor(hexString: "E4DDD6")
+        tableView.tableFooterView = UIView()
+        // cell setup
+        tableView.register(UINib(nibName: "RightViewCell", bundle: nil), forCellReuseIdentifier: "RightViewCell")
+        tableView.register(UINib(nibName: "LeftViewCell", bundle: nil), forCellReuseIdentifier: "LeftViewCell")
+        
+    }
+    
+    func fetchData() {
+        messages = MessageStore.getAll()
+        tableView.reloadData()
+    }
+}
 
-
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let message = messages[indexPath.row]
+        if message.side == .left {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "LeftViewCell") as! LeftViewCell
+            cell.configureCell(message: message)
+            return cell
+        }
+        else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "RightViewCell") as! RightViewCell
+            cell.configureCell(message: message)
+            return cell
+        }
+    }
+    
 }
 
